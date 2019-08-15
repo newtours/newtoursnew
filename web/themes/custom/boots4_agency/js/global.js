40c662169affac3324f421690d2593874916a7a3
Roman Bensman <rbensman@Roman-Mac-Pro.local>
2019-08-15 16:30:27 -0400

    Aug 15

--- web/themes/custom/boots4_agency/js/global.js
+++ web/themes/custom/boots4_agency/js/global.js
@@ -28,6 +28,35 @@
         position = scroll;
       });
 
+        function theme_menu(){
+
+            //Main menu
+            $('#main-menu').smartmenus();
+
+            //Mobile menu toggle
+            $('.navbar-toggle').click(function(){
+                $('.region-primary-menu').slideToggle();
+            });
+
+            //Mobile dropdown menu
+            if ( $(window).width() < 767) {
+                $(".region-primary-menu li a:not(.has-submenu)").click(function () {
+                    $('.region-primary-menu').hide();
+                });
+            }
+
+        }
+
+        function theme_home(){
+
+            //flexslider
+            $('.flexslider').flexslider({
+                animation: "slide"
+            });
+
+        }
+        theme_menu();
+        //theme_home();
     }
   };
 
