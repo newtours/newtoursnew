aa958edd225ff9edc5901989b58db75282f1798c
Roman Bensman <rbensman@Roman-Mac-Pro.local>
2019-11-01 16:04:27 -0400

    ControllerCommerce Product properties

--- web/modules/custom/tour_commerce/src/Controller/TourCommerceController.php
+++ web/modules/custom/tour_commerce/src/Controller/TourCommerceController.php
@@ -22,6 +22,8 @@ class TourCommerceController extends ControllerBase {
         $this->toursData = $toursData;
         $this->nodeStorage = $this->entityTypeManager()->getStorage('node');
         $this->ProductStorage = $this->entityTypeManager()->getStorage('commerce_product');
+        $this->ProductVariationStorage = $this->entityTypeManager()->getStorage('commerce_product_variation');
+        $this->store = $store = \Drupal\commerce_store\Entity\Store::load(1);
     }
 
 
@@ -35,45 +37,78 @@ class TourCommerceController extends ControllerBase {
     }
 
   /**
+   * https://www.drupal.org/project/commerce/issues/2811529
    * Createproduct.
    *
    * @return string
    *   Return Hello string.
    *
    * commerce_product
+   *
+   *   product is a storage of commerce_product
+   *   variation is storage of commerce_product_variation
+   *   price is a core/lib/Drupal/Core/Field/FieldItemList
    */
   public function createProduct($tour = null) {
 
 $tour = 65;
 //var_dump($entity_ids);exit;
       $tourData = $this->toursData->getTour($tour);
+
       $tourNodeId = key($tourData);
-      //var_dump($tourData[$tourNodeId]['tour_prices'][$tour][0]['price_main']);exit;
+      //var_dump($tourData[$tourNodeId]['tour_prices'][$tour][0]['price_nodeid']);exit;
       $nTours = count($tourData);
 
       $product = $this->ProductStorage
           ->loadByProperties([
               //'type' => 'commerce_product',
               'field_order_tour_tour' => $tourNodeId,
-          ]);
+          ]);//var_dump($product);exit;
+
+
+      //var_dump($productPrice[6]);exit;
+      //echo '<br/>';
+      //var_dump($product);exit;
       if (count($product) > 0 ) {
-          var_dump($product);
+          //var_dump($product[key($product)]->getVariationIds());exit;
+          $productVariationIds = $product[key($product)]->getVariationIds();
+          $productVariations = $this->ProductVariationStorage
+              ->loadByProperties([
+                  //'type' => 'commerce_product',
+                  'variation_id' => $productVariationIds,
+              ]);
+          if (count($productVariations) > 0 ) {
+              foreach ($productVariations as $key=>$value) {
+                  $productPrice[$key] = [
+                      'value'=> $value->get('price')->getValue()
+                  ];
+              }
+          }
       } else {
           // Create variation
+          $variation[] = $this->createProductVariation($tourData[$tourNodeId]['tour_prices'][$tour][0]['price_main']);
+          if(isset($tourData[$tourNodeId]['tour_prices'][$tour][0]['price_child'])) {
+
+          }
+
+          /**
           $variation = [
               ProductVariation::create([
-              'type' => 'default',
-              'sku' => 'var1',
+              'type' => 'price_main',
+              'sku' => 'price_maian',
               'price' => new Price($tourData[$tourNodeId]['tour_prices'][$tour][0]['price_main'], 'USD'),
+
           ]),
           ];
-
+           */
+//var_dump($variation);exit;
           // Create product using variations previously saved
           $product = Product::create([
-              'type' => 'default',
+              'type' => 'tour',
               'title' => t($tourData[$tourNodeId]['tour_name']),
               'field_order_tour_tour' => $tourNodeId,
               'variations' => $variation,
+              'field_product_type_price' =>$tourData[$tourNodeId]['tour_prices'][$tour][0]['price_nodeid'],
           ]);
           $product->save();
       }
@@ -83,4 +118,102 @@ $tour = 65;
     ];
   }
 
+
+    public function createProductVariation ($price, $attribute = [])
+    {
+
+       // The price of the variation.
+        if(0 == count($attribute)) {
+            $price = new Price($price, 'USD');
+            // Default variotion
+            $variation = ProductVariation::create([
+                'type' => 'default', // The default variation type is 'default'.
+                'sku' => 'tour_price', // The variation sku.
+                'status' => 1, // The product status. 0 for disabled, 1 for enabled.
+                'price' => $price,
+            ]);
+            $variation->save();
+        } else {
+
+            // Now we make the variation like we did above, except we also have to state the attribute for color.
+            $variation = \Drupal\commerce_product\Entity\ProductVariation::create([
+                'type' => $attribute['variation_type'], // The default variation type is 'default'.
+                'sku' => 'test-product-01', // The variation sku.
+                'status' => 1, // The product status. 0 for disabled, 1 for enabled.
+                'price' => $price,
+                'attribute_color' => $attribute['attribute'], // Set the attribute_color to the value entity, can use id as well.
+            ]);
+            $variation->save();
+        }
+        return $variation;
+
+    }
+
+    public function createProductType ($id)
+    {
+        $productType = \Drupal\commerce_product\Entity\ProductType::create([
+            'id' => $id,
+            'label' => 'Product Type with Price Variations',
+            'status' => 1, // 0 for disabled, 1 for enabled
+            'description' => 'Tour Prices Variations', // Optional
+            'variationType' => 'variation_type_with_color', // The variation type we want to reference for this
+            'injectVariationFields' => TRUE, // Optional - defaults to true
+        ]);
+        $productType->save();
+
+// These three functions must be called to add the appropriate fields to the type
+        commerce_product_add_variations_field($productType);
+        commerce_product_add_stores_field($productType);
+        commerce_product_add_body_field($productType);
+    }
+
+    protected function _createProductVariationType ($variationType = 'variation_type_tour_prices')
+    {
+// Create the new variation type.
+        $variationType = \Drupal\commerce_product\Entity\ProductVariationType::create([
+            'status' => 1, // Status, 0 for disabled, 1 for enabled.
+            'id' => '$variationType',
+            'label' => 'Variation for ' ,  $variationType,
+            'orderItemType' => 'default', // Order item type to reference. Default is 'default'.
+            'generateTitle' => TRUE, // True to generate titles based off of attribute values.
+        ]);
+        $variationType->save();
+
+// The actual attribute we want to make. Super creative 'color' being used as an example.
+        $priceAttribute = \Drupal\commerce_product\Entity\ProductAttribute::create([
+            'id' => 'prices',
+            'label' => 'Product Attribute Tour Prices ',
+        ]);
+        $priceAttribute->save();
+
+// The service that adds the attribute to the variation type.
+        $fieldManager = \Drupal::service('commerce_product.attribute_field_manager');
+        $fieldManager->createField($priceAttribute, $variationType->id());
+
+// Making the individual attribute values for 'color'.
+        $regular = $this->_createProductVarationAttribute ( 'regular' , $attribute = 'prices');
+        $child = $this->_createProductVarationAttribute ( 'child' , $attribute = 'prices');
+        $teen = $this->_createProductVarationAttribute ( 'teen' , $attribute = 'prices');
+
+
+    }
+
+
+    /**
+     * @param $name
+     * Possible value: 'regular', 'child', 'teen'
+     * @param string $attribute
+     * @return \Drupal\Core\Entity\EntityInterface|string|static
+     */
+    protected function _createProductVarationAttribute ( $name , $attribute = 'prices')
+    {
+
+        $attribute = \Drupal\commerce_product\Entity\ProductAttributeValue::create([
+            'attribute' => $attribute, // The attribute we are referencing.
+            'name' => $name, // Name of the actual value.
+        ]);
+        $attribute->save();
+        return $attribute;
+
+    }
 }
